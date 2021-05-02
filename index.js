5f4f284d7d406c7ace4a4b4f1c2c71fd601278db
coderaduo <coderaduo@gmail.com>
2021-05-02 13:03:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -187,6 +187,7 @@ mongoose.connection.on("opening", function () {
 
 mongoose.connection.on("disconnected", function () {
   console.error(new Error("Could not connect to mongo database"));
+  process.exit(exitCode);
   //mongoose.connect(connectionstring,{server:{auto_reconnect:true}});
 });
 
